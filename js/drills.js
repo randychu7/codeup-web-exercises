342a1f544f32e2fc6c63f58fa416e8c1650487dd
Randy Chu <randychu7@gmail.com>
2022-12-11 13:33:24 -0600

    added files over

--- js/drills.js
+++ js/drills.js
@@ -301,3 +301,10 @@
 // 3. Write a function, checkIfNumInArray, that takes in an array of numbers and a number and returns the string
 // ‘input num in input array’ if the second argument is an element in the first argument array
 // otherwise, the function should return the string ‘input num NOT in input array’
+
+function reverseWords(str) {
+    let reversedStr = str.split('').reverse().join('');
+    return reversedStr.split('').reverse().;
+
+}
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.'))

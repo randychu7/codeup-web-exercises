08bc470d5231256bded62fc29a7659c835ee8d9b
Randy Chu <randychu7@gmail.com>
2023-01-10 09:32:17 -0600

    lol

--- js/drills.js
+++ js/drills.js
@@ -660,3 +660,70 @@
 //   'b',
 //   25
 // 
+
+// ================================= WARM UP
+
+// Write a function, getUserCredentials, that takes in a list of user objects and returns a list of user objects with only the username and password properties. Assume at least an array of one user object.
+
+
+const users = [
+  {
+    firstName: 'Justin',
+    lastName: 'Reich',
+    dob: '1923-01-01',
+    username: 'jreich',
+    password: '$2y$10$UJlsa5vWq5DUKJjyO38gM.dCZfudWOFCrLWQosh0mhXKaZmRmvDse'
+  },
+  {
+    firstName: 'Sally',
+    lastName: 'Smith',
+    dob: '1935-03-11',
+    username: 'ssmith',
+    password: '$2y$10$VaLGU5.7uQLr.eg6kSI9seOcP4JY4XktWt28I9JgblAGIDpkDXbya'
+  },
+  {
+    firstName: 'Fred',
+    lastName: 'Smith',
+    dob: '1999-01-21',
+    username: 'fsmith',
+    password: '$2y$10$3USt6Dl8TNMkeh0KioPnfeVpynAotXvSIJ5xrzAHragPEAWMYEBNS'
+  },
+];
+
+function getUserCredentials(users){
+    let userInfo = [];
+    for(let i = 0; i < users.length; i++){
+        if (users[i] === 'username'){
+            userInfo.push(users[i]);
+         }
+        }
+       
+
+        
+  
+    
+
+
+
+
+
+
+console.log(getUserCredentials(users));
+/*
+
+[
+  {
+    username: 'jreich',
+    password: '$2y$10$UJlsa5vWq5DUKJjyO38gM.dCZfudWOFCrLWQosh0mhXKaZmRmvDse'
+  },
+  {
+    username: 'ssmith',
+    password: '$2y$10$VaLGU5.7uQLr.eg6kSI9seOcP4JY4XktWt28I9JgblAGIDpkDXbya'
+  },
+  {
+    username: 'fsmith',
+    password: '$2y$10$3USt6Dl8TNMkeh0KioPnfeVpynAotXvSIJ5xrzAHragPEAWMYEBNS'
+  }
+]
+
+*/
